455cd45bc3f07177361b3f523a1e6d4f0cae787e
TaishiKimura <taishi.canada@gmail.com>
2024-03-29 00:03:24 +0800

    pushしまーす

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,29 +21,42 @@
     <div class="antialiased">
         
         <div class="main-box">
-            <h1>Log In</h1>
+            <h1>Home</h1>
             <form action="">
 
-               <div class="input-box">
+               {{-- <div class="input-box">
                 <span class="icon"><i class='bx bxs-envelope'></i></span>
                 <input type="email" required>
                 <label>Email</label>
-               </div>
+               </div> --}}
 
-               <div class="input-box">
+               {{-- <div class="input-box">
                 <span class="icon"><i class='bx bxs-lock-alt' ></i></span>
                 <input type="password" required>
                 <label>Password</label>
-            </div>
+                </div> --}}
 
-            <div class="check">
+            {{-- <div class="check">
                 <label><input type="checkbox">Remember me</label>
                 <a href="#">Forget Password</a>
-            </div>
+            </div> --}}
 
-            <button type="submit" class="btn">
+            {{-- <button type="submit" class="btn">
                 <a href="{{ route('login') }}" class="btn">Log in</a>
-            </button>
+                <a href="{{ route('login') }}" class="btn">Log in</a>
+
+            </button> --}}
+            <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
+                {{-- @auth --}}
+                    {{-- <a href="{{ url('/home') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Home</a> --}}
+                {{-- @else --}}
+                    <a href="{{ route('login') }}" class="btn">Log in</a>
+
+                    {{-- @if (Route::has('register'))
+                        <a href="{{ route('register') }}" class="register-link">Register</a>
+                    @endif
+                @endauth --}}
+            </div>
 
             <div class="register">
                 <p>Don't have an account?
