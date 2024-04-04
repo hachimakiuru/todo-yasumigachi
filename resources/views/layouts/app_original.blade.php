6213430ea2ce923128741d98552c9b7cc5aaa57e
TaishiKimura <taishi.canada@gmail.com>
2024-04-04 19:08:50 +0800

    メインでやっちゃった！

--- resources/views/layouts/app_original.blade.php
+++ resources/views/layouts/app_original.blade.php
@@ -18,6 +18,8 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
   <link rel="stylesheet" href="{{ asset('css/style.css') }}">
   <script src="{{ asset('js/app_ori.js') }}"></script>
+
+  
 </head>
  
 {{-- headerは使わない継承機能 --}}
